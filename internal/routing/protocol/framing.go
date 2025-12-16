730bff9a97b21549b43da61458090c6912b576ec
dariomba <dariomartinezbajo@gmail.com>
2025-12-16 22:48:52 +0100

    feat: implement DecodeFrame for reading client messages

--- internal/routing/protocol/framing.go
+++ internal/routing/protocol/framing.go
@@ -1,18 +1,51 @@
 package protocol
 
-import "io"
+import (
+	"encoding/binary"
+	"errors"
+	"io"
+)
+
+const MaxFrameSize = 10 * 1024 * 1024 // 10 MB
 
 func DecodeFrame(r io.Reader) (Request, error) {
-	// read length
-	// read type byte
-	// decode payload into the correct struct
-	return nil, nil
+	lenBuf := make([]byte, 4)
+	_, err := io.ReadFull(r, lenBuf)
+	if err != nil {
+		return nil, err
+	}
+	length := binary.BigEndian.Uint32(lenBuf)
+	if length == 0 || length > MaxFrameSize {
+		return nil, errors.New("invalid frame length")
+	}
+
+	typeBuf := make([]byte, 1)
+	_, err = io.ReadFull(r, typeBuf)
+	if err != nil {
+		return nil, err
+	}
+	msgType := MessageType(typeBuf[0])
+
+	payloadBuf := make([]byte, length-1)
+	_, err = io.ReadFull(r, payloadBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	switch msgType {
+	case MsgProduce:
+		return &ProduceRequest{Payload: payloadBuf}, nil
+	default:
+		return &ProduceRequest{}, nil
+	}
 }
 
-func WriteError(r io.Reader, err error) {
-	// handle error
+func WriteError(w io.Writer, err error) {
+	// write dummy error response
+	w.Write([]byte("error: " + err.Error() + "\n"))
 }
 
-func EncodeFrame(r io.Reader, response Response) {
-	// handle error
+func EncodeFrame(w io.Writer, response Response) {
+	// write dummy response
+	w.Write([]byte("message received\n"))
 }
